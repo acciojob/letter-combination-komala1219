544fc4a59c08c5cf120eac26a04a15e8b49eb8a8
komala1219 <123307320+komala1219@users.noreply.github.com>
2023-04-14 16:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 function letterCombinations(input_digit) {
   //Complete the function
+
+	const mapping = {
+    '0': '0',
+    '1': '1',
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  function backtrack(combination, next_digits, output) {
+    if (next_digits.length === 0) {
+      output.push(combination);
+    } else {
+      const digit = next_digits[0];
+      const letters = mapping[digit];
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, next_digits.slice(1), output);
+      }
+    }
+  }
+
+  if (input_digit.length === 0) {
+    return [];
+  } else {
+    const output = [];
+    backtrack('', input_digit, output);
+    return output.sort();
+  }
 }
 
 module.exports = letterCombinations;
